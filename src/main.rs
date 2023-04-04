e0a37df4e98de360a00dd6609aac8b4308e31b85
kreijstal <rainb@tfwno.gf>
2023-04-04 15:30:52 +0200

    clicks work, finally

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 use std::ptr::null_mut;
-use winapi::shared::{windef::{POINT, RECT},
-minwindef::{HIWORD,LOWORD}};
+use winapi::shared::{
+    minwindef::{HIWORD, LOWORD, LPARAM, LRESULT, UINT, WPARAM},
+    windef::{HWND, POINT, RECT},
+};
+
+use std::ffi::CString;
+use winapi::shared::basetsd::LONG_PTR;
+use winapi::um::wingdi::GetStockObject;
+use winapi::um::wingdi::SYSTEM_FONT;
+use winapi::um::winuser::GetSysColor;
+use winapi::um::winuser::COLOR_WINDOWTEXT;
+use winapi::um::winuser::GetDlgItem;
+use winapi::um::winuser::GetSysColorBrush;
+use winapi::um::winuser::SendMessageA;
+use winapi::um::winuser::SetClassLongPtrA;
+use winapi::um::winuser::COLOR_WINDOW;
+use winapi::um::winuser::GCLP_HBRBACKGROUND;
+use winapi::um::winuser::WM_SETFONT;
 
 use winapi::um::winuser::{
-    CreateWindowExA, DefWindowProcA, GetMessageA, PostQuitMessage, RegisterClassA, ShowWindow,
-    TranslateMessage, DispatchMessageA, MSG, WM_DESTROY, WNDCLASSA, SW_SHOW, CW_USEDEFAULT,
-    WS_OVERLAPPEDWINDOW, WS_VISIBLE, WS_CHILD, ES_AUTOHSCROLL, BS_DEFPUSHBUTTON, BS_PUSHBUTTON,
-    IDCANCEL, IDOK, WM_COMMAND,
+    CreateWindowExA, DefWindowProcA, DispatchMessageA, GetMessageA, PostQuitMessage,
+    RegisterClassA, ShowWindow, TranslateMessage, BS_DEFPUSHBUTTON, BS_PUSHBUTTON, CW_USEDEFAULT,
+    ES_AUTOHSCROLL, IDCANCEL, IDOK, MSG, SW_SHOW, WM_COMMAND, WM_DESTROY, WNDCLASSA, WS_CHILD,
+    WS_OVERLAPPEDWINDOW, WS_SYSMENU, WS_VISIBLE,
 };
+extern "system" fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    match msg {
+        WM_COMMAND => {
+            let id = LOWORD(wparam as u32) as usize;
+            let code = HIWORD(wparam as u32);
+            let control_hwnd = lparam as HWND;
+            if code == 0 && id == IDOK as usize && control_hwnd == unsafe { GetDlgItem(hwnd, IDOK) }
+            {
+                println!("Ok clicked!");
+            } else if code == 0
+                && id == IDCANCEL as usize
+                && control_hwnd == unsafe { GetDlgItem(hwnd, IDCANCEL) }
+            {
+                println!("Cancel Clicked");
+            }
+            0
+        }
+        WM_DESTROY => {
+            unsafe { PostQuitMessage(0) };
+            0
+        }
+        _ => unsafe { DefWindowProcA(hwnd, msg, wparam, lparam) },
+    }
+}
 fn main() {
     unsafe {
         let class_name = "MyWindowClass\0".as_ptr() as *const i8;
-
+        //let class_name = CString::new("MyWindowClass").unwrap();
         // Register a window class
         let wnd_class = WNDCLASSA {
             style: 0,
-            lpfnWndProc: Some(DefWindowProcA),
+            lpfnWndProc: Some(window_proc), //Some(DefWindowProcA),
             hInstance: null_mut(),
             lpszClassName: class_name,
             cbClsExtra: 0,
@@ -32,7 +72,7 @@ fn main() {
             0,
             class_name,
             "Hello World\0".as_ptr() as *const i8,
-            WS_OVERLAPPEDWINDOW,
+            WS_OVERLAPPEDWINDOW | WS_SYSMENU,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
             640,
@@ -70,7 +110,7 @@ fn main() {
             80,
             20,
             hwnd_main,
-            null_mut(),
+            IDOK as isize as *mut _,
             null_mut(),
             null_mut(),
         );
@@ -86,11 +126,22 @@ fn main() {
             80,
             20,
             hwnd_main,
-            null_mut(),
+            IDCANCEL as isize as *mut _,
             null_mut(),
             null_mut(),
         );
+       /* let hfont = GetStockObject(SYSTEM_FONT);
+        SendMessageA(hwnd_text, WM_SETFONT, hfont as WPARAM, 0);
+        let hbr = GetSysColorBrush(COLOR_WINDOW);
+        SetClassLongPtrA(hwnd_main, GCLP_HBRBACKGROUND, hbr as LONG_PTR);
+        SendMessageA(hwnd_text, EM_SETBKGNDCOLOR, 0, COLOR_WINDOW as LPARAM);
 
+        let hfont = GetStockObject(SYSTEM_FONT);
+        SendMessageA(hwnd_text, WM_SETFONT, hfont as WPARAM, 0);
+
+        let hcolor = GetSysColor(COLOR_WINDOWTEXT);
+        SendMessageA(hwnd_text, EM_SETTEXTCOLOR, hcolor as WPARAM, 0);
+*/
         // Show the main window
         ShowWindow(hwnd_main, SW_SHOW);
 
@@ -110,31 +161,8 @@ fn main() {
             } else if result == -1 {
                 //handle error
             } else {
-            if msg.message == WM_COMMAND {
-                let id = LOWORD(msg.wParam as u32) as usize;
-                let code = HIWORD(msg.wParam as u32);
-                let hwnd = msg.hwnd;
-
-                if code == 0 && id == IDOK as usize && hwnd == hwnd_main {
-                    // OK button clicked
-                    let mut text_buf = [0u8; 1024];
-                    let text_len = winapi::um::winuser::GetWindowTextA(
-                        hwnd_text,
-                        text_buf.as_mut_ptr() as *mut i8,
-                        text_buf.len() as i32,
-                    );
-                    let text = std::str::from_utf8_unchecked(&text_buf[..text_len as usize]);
-                    println!("OK clicked with text: {}", text);
-                    break;
-                } else if code == 0 && id == IDCANCEL as usize && hwnd == hwnd_main {
-                    // Cancel button clicked
-                    println!("Cancel clicked");
-                    break;
-                }
-            } else {
-                    TranslateMessage(&msg);
-                    DispatchMessageA(&msg);
-                }
+                TranslateMessage(&msg);
+                DispatchMessageA(&msg);
             }
         }
 
