6c13019fd89249f904258f3ff4d8fb5373bf54f5
kreij <kreij@LAPTOP-31E3HV7R>
2023-04-04 20:41:02 +0200

    Fix font issue

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,89 @@
-use windows::{Win32::{
-    Foundation::*,
-    UI::{WindowsAndMessaging::*,Controls::RichEdit::*},
-	Graphics::Gdi::*,
-	System::LibraryLoader::*,
-},core::*,w};
+use windows::{
+    core::*,
+    w,
+    Win32::{
+        Foundation::*,
+        Graphics::Gdi::*,
+        System::{LibraryLoader::*, WindowsProgramming::*},
+        UI::{Controls::RichEdit::*, WindowsAndMessaging::*},
+    },
+};
 
+pub fn LOWORD(l: usize) -> usize {
+    l & 0xffff
+}
 
-/*
-Foundation::{LPARAM, LRESULT, POINT, RECT, WPARAM,HWND},
-UI::WindowsAndMessaging::{
-        BS_DEFPUSHBUTTON, BS_PUSHBUTTON, CreateWindowExW, CW_USEDEFAULT,
-        DefWindowProcW, DispatchMessageW, ES_AUTOHSCROLL, ES_LEFT, GCLP_HBRBACKGROUND, GetClientRect,
-        */
-use windows::Win32::UI::WindowsAndMessaging::WNDCLASSW;
-use std::mem::{size_of, zeroed};
+pub fn HIWORD(l: usize) -> usize {
+    (l >> 16) & 0xffff
+}
+unsafe extern "system" fn window_proc(
+    hwnd: HWND,
+    msg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    match msg {
+        WM_COMMAND => {
+            let control_id = LOWORD(wparam.0);
+            let notification_code = HIWORD(wparam.0);
+            let control_handle = lparam;
+
+            if control_id == IDOK.0 as usize
+                && notification_code == 0
+                && control_handle.0 == GetDlgItem(hwnd, IDOK.0).0
+            {
+                println!("OK button clicked!");
+            } else if control_id == IDCANCEL.0 as usize
+                && notification_code == 0
+                && control_handle.0 == GetDlgItem(hwnd, IDCANCEL.0).0
+            {
+                println!("Cancel button clicked!");
+            }
+        }
+        WM_DESTROY => {
+            PostQuitMessage(0);
+        }
+        _ => {
+            return DefWindowProcW(hwnd, msg, wparam, lparam);
+        }
+    }
+
+    return LRESULT(0);
+}
 
 fn main() {
-	
-	// Get the HINSTANCE for the current process
-let h_instance = unsafe { GetModuleHandleW(None).unwrap() };
+    // Get the HINSTANCE for the current process
+    let h_instance = unsafe { GetModuleHandleW(None).unwrap() };
 
-// Load the default application icon
-let h_icon = unsafe { LoadIconW(None, IDI_APPLICATION).unwrap() };
+    // Load the default application icon
+    let h_icon = unsafe { LoadIconW(None, IDI_APPLICATION).unwrap() };
 
-// Load the default arrow cursor
-let h_cursor = unsafe { LoadCursorW(None, IDC_ARROW).unwrap() };
+    // Load the default arrow cursor
+    let h_cursor = unsafe { LoadCursorW(None, IDC_ARROW).unwrap() };
 
-// Register a window class
-//let class_name = "MyWindowClass\0".encode_utf16().collect::<Vec<u16>>();
-let class_name = w!("MyWindowClass");
-let wnd_class = WNDCLASSW {
-    style: WNDCLASS_STYLES(0),
-    lpfnWndProc: Some(window_proc),
-    hInstance: h_instance,
-    lpszClassName: windows::core::PCWSTR(class_name.as_ptr()),
-    cbClsExtra: 0,
-    cbWndExtra: 0,
-    hIcon: h_icon,
-    hCursor: h_cursor,
-    hbrBackground: unsafe { GetSysColorBrush(COLOR_WINDOW) },
-    lpszMenuName: PCWSTR::null(),
-};
+    // Register a window class
+    //let class_name = "MyWindowClass\0".encode_utf16().collect::<Vec<u16>>();
+    let class_name = w!("MyWindowClass");
+    let wnd_class = WNDCLASSW {
+        style: WNDCLASS_STYLES(0),
+        lpfnWndProc: Some(window_proc),
+        hInstance: h_instance,
+        lpszClassName: windows::core::PCWSTR(class_name.as_ptr()),
+        cbClsExtra: 0,
+        cbWndExtra: 0,
+        hIcon: h_icon,
+        hCursor: h_cursor,
+        hbrBackground: unsafe { GetSysColorBrush(COLOR_WINDOW) },
+        lpszMenuName: PCWSTR::null(),
+    };
     unsafe {
-       
         RegisterClassW(&wnd_class);
 
         // Create the main window
         let hwnd_main = CreateWindowExW(
             WINDOW_EX_STYLE(0),
             class_name,
-            w!("Hello World")
+            w!("Only 1 letter in title"),
             WS_OVERLAPPEDWINDOW | WS_SYSMENU,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
@@ -64,14 +98,17 @@ let wnd_class = WNDCLASSW {
         // Create the text box
         let hwnd_text = CreateWindowExW(
             WINDOW_EX_STYLE(0),
-            w!("Edit"),
+            w!("EDIT"),
             w!(""),
-            WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL,
+            WS_CHILD
+                | WS_VISIBLE
+                | WINDOW_STYLE(ES_LEFT as u32)
+                | WINDOW_STYLE(ES_AUTOHSCROLL as u32),
             10,
             10,
             300,
             20,
-            HWND(0),
+            hwnd_main,
             HMENU(0),
             h_instance,
             None,
@@ -80,45 +117,108 @@ let wnd_class = WNDCLASSW {
         // Create the OK button
         let hwnd_ok = CreateWindowExW(
             WINDOW_EX_STYLE(0),
-            w!("Button"),
+            w!("BUTTON"),
             w!("Ok"),
-            WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            WS_VISIBLE | WS_CHILD | WINDOW_STYLE(BS_DEFPUSHBUTTON as u32),
             320,
             10,
             80,
             20,
-            HWND(0),
-            HMENU(IDOK),
+            hwnd_main,
+            HMENU(IDOK.0 as isize),
             h_instance,
             None,
         );
 
         // Create the Cancel button
         let hwnd_cancel = CreateWindowExW(
-            0,
-            Button::get_window_class_name().as_ptr(),
-            "Cancel\0".encode_utf16().as_ptr(),
-            WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            WINDOW_EX_STYLE(0),
+            w!("BUTTON"),
+            w!("Cancel"),
+            WS_VISIBLE | WS_CHILD | WINDOW_STYLE(BS_DEFPUSHBUTTON as u32),
             410,
             10,
             80,
             20,
             hwnd_main,
-            IDCANCEL as isize as HMENU,
+            HMENU(IDCANCEL.0 as isize),
             h_instance,
-            std::ptr::null_mut(),
+            None,
         );
 
         // Show the main window
         ShowWindow(hwnd_main, SW_SHOW);
+        // Retrieve a handle to the system color brush for the window background
+        let hbr = unsafe { GetSysColorBrush(COLOR_WINDOW) };
+        let mut non_client_metrics = NONCLIENTMETRICSW::default();
+        non_client_metrics.cbSize = std::mem::size_of::<NONCLIENTMETRICSW>() as u32;
+        let result = unsafe {
+            use core::ffi::c_void;
+            SystemParametersInfoW(
+                SPI_GETNONCLIENTMETRICS,
+                non_client_metrics.cbSize,
+                Some(&mut non_client_metrics as *mut _ as *mut c_void),
+                SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS(0),
+            )
+        };
+        let message_log_font = if result.as_bool() {
+            let message_log_font: LOGFONTW = non_client_metrics.lfMessageFont;
+            // You now have the LOGFONTW for the current font in message_log_font.
+            message_log_font
+        } else {
+            // Handle the error case.
+            // Define the font name and size
+            let font_name = w!("Tahoma");
+            let hdc = unsafe { GetDC(hwnd_main) };
+            let font_size = -MulDiv(8, GetDeviceCaps(hdc, LOGPIXELSY), 72);
+            unsafe {
+                ReleaseDC(hwnd_main, hdc);
+            };
+            // Create a new LOGFONTW struct for the font
+            let mut log_font = LOGFONTW::default();
+            log_font.lfHeight = font_size;
+            unsafe {
+                std::ptr::copy_nonoverlapping(
+                    font_name.0,
+                    log_font.lfFaceName.as_mut_ptr(),
+                    log_font.lfFaceName.len().min(wcslen(font_name)),
+                );
+            }
+            // Create a handle to the font
+            log_font
+        };
+        let hfont = unsafe { CreateFontIndirectW(&message_log_font) };
+
+        // Retrieve a handle to the system font
+        //let hfont = unsafe { GetStockObject(SYSTEM_FONT) };
+
+        // Retrieve the system color for the window text
+        let hcolor = unsafe { GetSysColor(COLOR_WINDOWTEXT) };
+
+        unsafe {
+            // Set the background color for the main window class
+            SetClassLongPtrW(hwnd_main, GCLP_HBRBACKGROUND, hbr.0);
+
+            SendMessageW(hwnd_text, WM_SETFONT, WPARAM(hfont.0 as usize), LPARAM(0));
+            SendMessageW(hwnd_ok, WM_SETFONT, WPARAM(hfont.0 as usize), LPARAM(0));
+            SendMessageW(hwnd_cancel, WM_SETFONT, WPARAM(hfont.0 as usize), LPARAM(0));
+
+            // Set the background color for the text control
+            SendMessageW(
+                hwnd_text,
+                EM_SETBKGNDCOLOR,
+                WPARAM(COLOR_WINDOW.0 as usize),
+                LPARAM(hbr.0),
+            );
+        }
 
         // Run the message loop
         let mut msg = MSG::default();
         loop {
-            let result = GetMessageW(&mut msg, HWND::NULL, 0, 0);
-            if result == 0 {
+            let result = GetMessageW(&mut msg, HWND(0), 0, 0);
+            if result == BOOL(0) {
                 break;
-            } else if result == -1 {
+            } else if result == BOOL(-1) {
                 //handle error
             } else {
                 TranslateMessage(&msg);
@@ -129,46 +229,4 @@ let wnd_class = WNDCLASSW {
         // Clean up
         PostQuitMessage(0);
     }
-}
-
-unsafe extern "system" fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    match msg {
-        WM_CREATE => {
-            let h_instance = GetModuleHandleW(None);
-            let h_brush = CreateSolidBrush(COLOR_WINDOW);
-            SetClassLongPtrW(hwnd, GCLP_HBRBACKGROUND, h_brush.0 as isize);
-            let hwnd_text = CreateWindowExW(
-                0,
-                Edit::get_window_class_name().as_ptr(),
-                "".encode_utf16().as_ptr(),
-                WS_CHILD | WS_VISIBLE | ES_LEFT | ES_AUTOHSCROLL,
-                10,
-                10,
-                300,
-                20,
-                hwnd,
-                HMENU::NULL,
-                h_instance,
-                std::ptr::null_mut(),
-            );
-            SendMessageW(hwnd_text, EM_SETBKGNDCOLOR, 0, COLOR_WINDOW as isize);
-        }
-        WM_PAINT => {
-            let mut ps: PAINTSTRUCT = zeroed();
-            let hdc = BeginPaint(hwnd, &mut ps);
-            let rect = RECT {
-                left: ps.rcPaint.left,
-                top: ps.rcPaint.top,
-                right: ps.rcPaint.right,
-                bottom: ps.rcPaint.bottom,
-            };
-            FillRect(hdc, &rect, GetSysColorBrush(COLOR_WINDOW));
-            EndPaint(hwnd, &ps);
-        }
-        WM_DESTROY => {
-            PostQuitMessage(0);
-        }
-        _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
-    }
-    0
 }
